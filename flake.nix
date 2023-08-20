2cacde8c518b3550474e3dba1738fcd8d81593cb
neocrz <fernandogomescruz@gmail.com>
2023-08-20 16:53:31 -0300

    .

--- flake.nix
+++ flake.nix
@@ -1,15 +1,20 @@
 {
-  description = "my project description";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  description = "Flake utils demo";
+
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShells.default = import ./shell.nix { inherit pkgs; };
-          packages.default = import ./package.nix { inherit pkgs; };
-        }
-      );
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        packages = rec {
+          hello = pkgs.hello;
+          default = hello;
+        };
+        apps = rec {
+          hello = flake-utils.lib.mkApp { drv = self.packages.${system}.hello; };
+          default = hello;
+        };
+      }
+    );
 }
